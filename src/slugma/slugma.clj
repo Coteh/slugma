a54152104d657c34997a801542eb64dca2692b00
Coteh <james_c_34@live.com>
2017-07-12 23:45:48 -0400

    Remove accented characters.

--- src/slugma/slugma.clj
+++ src/slugma/slugma.clj
@@ -1,9 +1,22 @@
 (ns slugma.slugma
   (:require [clojure.string :as str]))
 
+;;;; remove-diacritics
+;;; Breaks up accented characters into regular character + diacritic
+;;; Filters out nonspacing (Mn) and enclosing (Me) diacritics
+(defn remove-diacritics [str]
+  (-> str
+    (java.text.Normalizer/normalize java.text.Normalizer$Form/NFD)
+    (str/replace #"[\p{Mn}\p{Me}]+" "")))
+
+;;;; slugma
+;;; The main slugma module function
+;;; Performs some filtering to get the slugged
+;;; text we want
 (defn slugma [line]
   (def result
     (-> line
+      remove-diacritics
       (str/replace #"[^A-Za-z0-9]+" "-")
       str/lower-case))
 
